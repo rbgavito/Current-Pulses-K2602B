fec125df25dc1a6a8af2c5af2206be70e74b629c
rbgavito <32744667+rbgavito@users.noreply.github.com>
2017-10-20 14:31:21 +0100

    Update pulseimeasureiv.py

--- pulseimeasureiv.py
+++ pulseimeasureiv.py
@@ -6,6 +6,11 @@ import numpy as np
 def exportFile(filename, data):
   with open(filename, 'wt') as csvfile:
     csvfile.write(data)
+    
+def openCommPort(IP):
+  #Open communication ports
+  rm = visa.ResourceManages('@py')
+  instr = rm.open_resource('TCPIP::' + IP + '::INSTR')
  
 limitv = 0.9 #Maximum voltage limit (in volts)
 limiti = 0.001 #Maximum current limit (in amperes)
@@ -13,21 +18,21 @@ v = '' #Will hold the voltage readings
 i = '' #Will hold the current readings
 pulses = 100 #Number of pulses per train
 loops = 10 #Number of pulse trains
-delay = 0.02 #Delay before measuring voltage (in seconds)
+width = 0.02 #Pulse width (in seconds)
 milliamps = 0.308  #Current level of the pulses (in milliamps)
 nplc = 0.005  #Number of power cycles for integration
 
-#Open communication ports
-rm = visa.ResourceManages('@py')
-instr = rm.open_resource('TCPIP::169.254.0.1::INSTR')
+openCommPort('169.254.0.1')
+
 
 #Initialise SMU A
 instr.write("smua.reset()")
 instr.write("smua.source.func = smua.OUTPUT_DCAMPS") #SMU A will act as a current source and voltmeter
 instr.write("smua.source.limitv = " + str(limitv)+ "")
 instr.write("smua.nvbuffer1.appendmode = 1")  #New readings are appended to the existing data
-instr.write("smua.measure.delay = " + str(delay) + "")
 instr.write("smua.measure.nplc = " + str(nplc) + "")
+instr.write("smua.measure.autozero = smua.AUTOZERO_ONCE")
+instr.write("smua.source.rangei = 1e-3")
 
 #Initialise SMU B
 instr.write("smub.reset()")
@@ -35,6 +40,26 @@ instr.write("smub.source.func = smua.OUTPUT_DCVOLTS")  #SMU B will act as ampmet
 instr.write("smub.source.limiti = " + str(limiti)+ "")
 instr.write("smub.nvbuffer1.appendmode = 1")  #New readings are appended to the existing data
 instr.write("smub.measure.nplc = " + str(nplc) + "")
+instr.write("smub.measure.autozero = smub.AUTOZERO_ONCE")
+
+#Configure pulse
+instr.write("smua.trigger.source.action = smua.ENABLE")
+instr.write("smua.trigger.measure.action = smua.ENABLE")
+instr.write("smua.trigger.measure.v(smua.nvbuffer1)")
+
+instr.write("trigger.timer[1].count = 1")
+instr.write("trigger.timer[1].passthrough = false")
+instr.write("trigger.timer[1].stimulus = smua.trigger.ARMED_EVENT_ID")
+instr.write("smua.trigger.source.stimulus = 0")
+instr.write("smua.trigger.measure.stimulus = trigger.timer[1].EVENT_ID")
+instr.write("smua.trigger.endpulse.stimulus = trigger.timer[1].EVENT_ID")
+instr.write("smua.trigger.endpulse.action = smua.SOURCE_IDLE")
+instr.write("smua.trigger.count = 1")
+instr.write("smua.trigger.arm.count = 1")
+
+instr.write("smua.source.trigger.listi({" + str(milliamps) + "e-6})")
+inst.write("trigger.timer[1].delay = " + str(width) + "")
+
 
 #Turn both sources on
 instr.write("smua.source.output = smua.OUTPUT_ON")
@@ -47,12 +72,12 @@ for l in range(0, loops):
   instr.write("waitcomplete()") #Wait for the buffers to be cleared. Not sure if necessary
   
   #For loop creating the pulse train at a fixed current. Horribly long.
-  instr.write("for a = 1,100 do smua.source.leveli=" + str(milliamps) + "e-3 smua.measurev(smua.nvbuffer1) smub.measurei(smub.nvbuffer1) smua.source.leveli=0 end"))
+  instr.write("for a = 1,100 do smua.trigger.initiate() smub.measure.i(smub.nvbuffer1) waitcomplete() end"))
   instr.write("waitcomplete()") #Again, not sure if necessary 
   
   v = v + instr.query("x = printbuffer(1,smua.nvbuffer1.n,smua.nvbuffer1.readings)")
   i = i + instr.query("x = printbuffer(1,smub.nvbuffer1.n,smub.nvbuffer1.readings)")
-  exportfile(filename + "-" + str(l) + ".txt", v + i)
+  exportfile(filename + "-" + str(milliamps) + "-" + str(l) + ".txt", v + i)
   
 #Turn both sources off
 instr.write("smua.source.output = smua.OUTPUT_OFF")
